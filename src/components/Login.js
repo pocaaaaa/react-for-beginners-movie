cc17415cf1941836d06fca2ae4faca5db505b0ad
hyunjung <space90220@gmail.com>
2023-08-13 01:48:45 +0900

    로그인 (진행중)

---
+++ src/components/Login.js
@@ -0,0 +1,132 @@
+import React, { useState, useRef } from 'react';
+import styled from "@emotion/styled";
+import { BiSolidCat } from "react-icons/bi";
+
+const LoginForm = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  width: 100%;
+  text-align: center;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const InputContainerLabel = styled.label`
+  flex: 1;
+  font-weight: bold;
+  text-align: right;
+  margin-right: 10px;
+`;
+
+const InputContainerInput = styled.input`
+  flex: 3;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 8px;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  background-color: black;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 12px;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  /* state */
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  /* reg */
+  const userIdRef = useRef();
+  const passwordRef = useRef();
+
+  /* function */
+  const getCookie = () => {
+
+  };
+
+  const setCookie = () => {
+    alert("쿠키저장!!");
+  };
+
+  const handleIdChange = (event) => {
+    setUserId(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const login = () => {
+    if(!userId) {
+      alert("Please enter your id.");
+      userIdRef.current.focus();
+      return false;
+    }
+
+    if(!password) {
+      alert("Please enter your password.");
+      passwordRef.current.focus();
+      return false;
+    }
+
+    let userList = localStorage.getItem('userList');
+    userList = userList ? JSON.parse(userList) : [];
+    userList = userList.filter(user => user.id === userId && user.password === password);
+    
+    if(userList && userList.length > 0) {
+      alert("사용자 있음!!!");
+      setCookie();
+    } else {
+      alert("사용자 없음!!!");
+    }
+  };
+
+  return (
+    <LoginForm>
+      <h2>Login<BiSolidCat/></h2>
+      <InputContainer>
+        <InputContainerLabel htmlFor="id">ID:</InputContainerLabel>
+        <InputContainerInput 
+          type="text" 
+          id="id"
+          value={userId}
+          ref={userIdRef}
+          onChange={handleIdChange}
+          required 
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputContainerLabel htmlFor="password">Password:</InputContainerLabel>
+        <InputContainerInput 
+          type="password" 
+          id="password"
+          value={password}
+          ref={passwordRef}
+          onChange={handlePasswordChange}
+          required 
+        />
+      </InputContainer>
+      <SubmitButton 
+        className="submit-button"
+        onClick={login}
+      >
+        Login
+      </SubmitButton>
+    </LoginForm>
+  );
+};
+
+export default Login;
--- src/routes/Review.js
+++ src/routes/Review.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import styles from "./Review.module.css";
 import Nav from "../components/Nav";
+import Login from "../components/Login";
+import styled from "@emotion/styled";
+
+const Body = styled.div`
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+`;
 
 const Review = () => {
   const [loading, setLoading] = useState(false);
@@ -14,9 +27,12 @@ const Review = () => {
       ) : (
         <div>
           <Nav />
-          <div className={styles.reviews__nodata}>
+          {/* <div className={styles.reviews__nodata}>
             준비중입니다.
-          </div>
+          </div> */}
+          <Body>
+            <Login />
+          </Body>
         </div>
       )}
     </div>
